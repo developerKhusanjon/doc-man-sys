048fdda0441849e75a868f6e383de7a6346a5adc
khusanjon <developerkhusanjon@gmail.com>
2023-02-06 14:11:10 +0500

    Core libs and health endpoint added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,28 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+
+import scala.concurrent.ExecutionContext
+import scala.io.StdIn
+
 object Main extends App {
-  println("Hello World!")
+  private implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "main-system")
+  protected implicit val executor: ExecutionContext = system.executionContext
+
+  val route =
+    path("health") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>200</h1>"))
+      }
+    }
+
+  val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+  println(s"Server now online. Please navigate to http://localhost:8080/health\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 }
