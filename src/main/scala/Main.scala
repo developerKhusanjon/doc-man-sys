bf0682c13b75bdf3c039ad7f5c1fb3c53b61102e
khusanjon <developerkhusanjon@gmail.com>
2023-02-06 13:54:14 +0500

    Empty

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,3 @@
-import akka.http.scaladsl.server.Directives._
-import akka.actor.ActorSystem
-import akka.event.{Logging, LoggingAdapter}
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.stream.ActorMaterializer
-
-import scala.concurrent.ExecutionContext
-import scala.io.StdIn
-
 object Main extends App {
-  private implicit val system: ActorSystem = ActorSystem()
-  protected implicit val executor: ExecutionContext = system.dispatcher
-  protected val log: LoggingAdapter = Logging(system, getClass)
-  protected implicit val materializer: ActorMaterializer = ActorMaterializer()
-
-  val route =
-    path("health") {
-      get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>200</h1>"))
-      }
-    }
-
-  val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
-
-  println(s"Server now online. Please navigate to http://localhost:8080/health\nPress RETURN to stop...")
-  StdIn.readLine()
-  bindingFuture
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
+  println("Hello World!")
 }
