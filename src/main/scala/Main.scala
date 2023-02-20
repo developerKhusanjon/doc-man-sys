591061463f6ad15c7b1c774e5a2bb4d0c5036cd4
khusanjon <developerkhusanjon@gmail.com>
2023-02-20 19:47:29 +0500

    Reverted

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,8 @@
-import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import dev.khusanjon.docmansys.migration.DatabaseMigrationActor
 
 import scala.concurrent.ExecutionContext
 import scala.io.StdIn
