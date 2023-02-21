3f21c8b2cd56a61cc6aae279d580fd074ba52b76
khusanjon <developerkhusanjon@gmail.com>
2023-02-21 16:23:42 +0500

    Flway and Postgres DB configs added

---
+++ src/main/scala/dev/khusanjon/docmansys/conf/MigrationConfig.scala
@@ -0,0 +1,15 @@
+package dev.khusanjon.docmansys.conf
+
+import org.flywaydb.core.Flyway
+
+trait MigrationConfig extends Config {
+
+  private val flyway = new Flyway(Flyway.configure())
+
+  def migrate(): Unit = flyway.migrate()
+
+  def reloadSchema(): Unit = {
+    flyway.clean()
+    flyway.migrate()
+  }
+}
